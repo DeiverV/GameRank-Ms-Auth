48f8a39fc67734ddd332f18c35a576ab6532b07b
DeiberV <deiberveranodev@gmail.com>
2024-10-25 08:46:56 -0500

    grpc client option

---
+++ src/grpc-client.options.ts
@@ -0,0 +1,11 @@
+import { ClientOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+export const grpcClientOptions: ClientOptions = {
+  transport: Transport.GRPC,
+  options: {
+    package: 'products',
+    protoPath: join(__dirname, 'proto/auth.proto'),
+    url: 'localhost:50051',
+  },
+};
